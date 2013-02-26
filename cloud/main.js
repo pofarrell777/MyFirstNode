9b55a83d566e36e4305ed77f62f1c9d47688906b
pofarrell@tssg.org <pofarrell@tssg.org>
2013-02-26 12:15:08 +0000

    update file /cloud/main.js

--- cloud/main.js
+++ cloud/main.js
@@ -6,11 +6,8 @@ exports.myFunction = function(params, callback) {
   callback(undefined, {result: num});
 };
 exports.myFunctionRed = function(callback) {
-  var num = params.num;
-  if(num > 10){
-    return callback({err: 'Num too large'}, {result: num});
-  }
-  callback(undefined, {result: num});
+    return callback({err: 'Num too large'});
+
 };
 exports.myFunctionGreen = function(params, callback) {
   var num = params.num;
